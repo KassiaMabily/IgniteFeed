141d90f988eb2372546ee5c861a2c348a915ecf6
Kassia Fraga <kassiafraga7@gmail.com>
2022-06-29 10:56:30 -0300

    Componente de sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import './global.css';
 import { Header } from './components/Header';
+import { Post } from './Post';
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author={"Diego Fernandez"}
+            content={"Lorem Ipsum"}
+          />
+
+          <Post
+            author={"Diego Fernandez"}
+            content={"Lorem Ipsum"}
+          />
+        </main>
+      </div>
     </div>
   )
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,5 +1,9 @@
-export function Post() {
-  <div>
-    <h1>Post</h1>
-  </div>
+export function Post(props) {
+
+  return (
+    <div>
+      <h1>{props.author}</h1>
+      <p>{props.content}</p>
+    </div>
+  )
 }
