e1de80946183d61033cfa0fb342e4a80a4c65f8a
Kassia Fraga <kassiafraga7@gmail.com>
2022-07-19 15:32:55 -0300

    Formulário de comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,6 +33,22 @@ export function Post() {
           <a href="#">#rocketseat</a>
         </p>
       </div>
+
+
+
+      <form className={styles.commentForm}>
+        <strong>Deixe o seu feedback</strong>
+
+        <textarea
+          placeholder='Deixe um comentario'
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+
+      </form>
+
     </article>
   )
 }
